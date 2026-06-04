615f7b39723a63c87ef1ca1c784b541406c393e5
Vinnu <vinnump08@gmail.com>
2026-06-04 09:17:29 +0530

    Changed baseURL of axios instance

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://orbita-wdyx.onrender.com",
+  baseURL: "https://orbita-wdyx.onrender.com/api/v1",
   withCredentials: true,
 });
 
