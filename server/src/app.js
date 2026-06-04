ac0d8a445268e574384e977136bf10ada48ba869
Vinnu <vinnump08@gmail.com>
2026-06-04 09:12:35 +0530

    Changed cors configuration

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [process.env.CORS_ORIGIN, "https://orbita-jade.vercel.app/"],
+    origin: [process.env.CORS_ORIGIN, "https://orbita-jade.vercel.app"],
     credentials: true,
   }),
 );
