b6d2f14c95282a66a2b6ec22da29fc18356d6785
Vinnu <vinnump08@gmail.com>
2026-06-04 09:08:49 +0530

    Added CORS configuration

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,12 @@ import User from "./models/user.model.js";
 
 const app = express();
 
-app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(
+  cors({
+    origin: [process.env.CORS_ORIGIN, "https://orbita-jade.vercel.app/"],
+    credentials: true,
+  }),
+);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
