2e5cbc18dbd681109bf428cbf096263d089054ef
Vinnu <vinnump08@gmail.com>
2026-06-23 10:50:30 +0530

    Email Verification solved.

--- server/src/services/email.config.js
+++ server/src/services/email.config.js
@@ -4,7 +4,11 @@ dotenv.config({ path: "./.env" });
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
